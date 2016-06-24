0663a9caadc61b46370df51e3a62f0040a712286
Marco <marcoaremail@gmail.com>
2016-06-24 12:16:13 -0300

    Header to manage includes

---
+++ graphics.h
@@ -0,0 +1,12 @@
+#ifndef MODULE_GRAPHICS_GEOMETRY_H_
+#define MODULE_GRAPHICS_GEOMETRY_H_
+
+#include "geometry/geometry.h"
+#include "pigment/bitmap.h"
+#include "pigment/color.h"
+#include "pigment/image.h"
+#include "pigment/procedural.h"
+#include "pigment/solid.h"
+#include "pigment/texture.h"
+
+#endif
