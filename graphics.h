7fdcd5464c3a2fd25ca4b821426958046a95d016
Marco <marcoaremail@gmail.com>
2016-06-24 13:39:04 -0300

    Including shape headers

--- graphics.h
+++ graphics.h
@@ -2,11 +2,20 @@
 #define MODULE_GRAPHICS_GEOMETRY_H_
 
 #include "geometry/geometry.h"
+
 #include "pigment/bitmap.h"
 #include "pigment/color.h"
 #include "pigment/image.h"
 #include "pigment/procedural.h"
 #include "pigment/solid.h"
 #include "pigment/texture.h"
+
+#include "shape/box.h"
+#include "shape/csg_tree.h"
+#include "shape/cylinder.h"
+#include "shape/polyhedron.h"
+#include "shape/shape.h"
+#include "shape/sphere.h"
+#include "shape/transformed_shape.h"
 
 #endif
