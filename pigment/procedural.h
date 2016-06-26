184c15e338ccf37d5fa9596beb562400ab52d02d
Marco <marcoaremail@gmail.com>
2016-06-26 02:27:52 -0300

    Base texture procedural class implementation

--- pigment/procedural.h
+++ pigment/procedural.h
@@ -12,10 +12,21 @@ namespace Pigment {
         std::function<Color(const Geometry::Vec<2> &param)> procedure;
 
     public:
-        Procedural (const std::function<Color(const Geometry::Vec<2> &param)> &_procedure) :
-            procedure(_procedure) {}
+        Procedural (
+            const std::function<Color(const Geometry::Vec<2> &param)> &_procedure,
+            const float_max_t &_s_size = 1.0,
+            const float_max_t &_t_size = 1.0,
+            const float_max_t &_s_offset = 0.0,
+            const float_max_t &_t_offset = 0.0
+        ) :
+            Texture(_s_size, _t_size, _s_offset, _t_offset), procedure(_procedure) {}
 
-        inline Color at (const Geometry::Vec<2> &param) override { return this->procedure(param); }
+        inline Color at (const Geometry::Vec<2> &param) const override {
+            return this->procedure({
+                param[0] * this->getInverseSSize() + this->getSOffset(),
+                param[1] * this->getInverseTSize() + this->getTOffset()
+            });
+        }
     };
 
 };
