96c3fd4fc14dbf03e49b8b8b5d1c18ca14619509
Marco <marcoaremail@gmail.com>
2016-06-26 02:25:29 -0300

    New constructors and cast based on opencv Vec3b

--- pigment/color.h
+++ pigment/color.h
@@ -1,6 +1,7 @@
 #ifndef MODULE_GRAPHICS_PIGMENT_COLOR_H_
 #define MODULE_GRAPHICS_PIGMENT_COLOR_H_
 
+#include <opencv2/opencv.hpp>
 #include "../geometry/defaults.h"
 #include "../geometry/vec.h"
 
@@ -14,6 +15,17 @@ namespace Pigment {
         inline static Color rgba (unsigned char red, unsigned char green, unsigned char blue, float_max_t alpha) { return Color(red / 255.0, green / 255.0, blue / 255.0, alpha); }
 
         Color (void) : Geometry::Vec<4>(0.0) {}
+        Color (const Vec<4> &_vec) : Geometry::Vec<4>(_vec) {}
+        Color (const Vec<3> &_vec) :
+            Geometry::Vec<4>({ _vec[0], _vec[1], _vec[2], 1.0 }) {}
+        Color (const cv::Vec3b &_vec) :
+            Geometry::Vec<4>({ _vec[2] / 255.0, _vec[1] / 255.0, _vec[0] / 255.0, 1.0 }) {}
+
+        operator cv::Vec3b (void) const { return {
+            static_cast<unsigned char>(this->blue() * 255.0),
+            static_cast<unsigned char>(this->green() * 255.0),
+            static_cast<unsigned char>(this->red() * 255.0)
+        }; }
 
         Color (float_max_t red, float_max_t green, float_max_t blue, float_max_t alpha = 1.0) :
             Geometry::Vec<4>({ red, green, blue, alpha }) {};
