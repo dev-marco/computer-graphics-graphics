a1ecd98db92038f99cca45138805c4c81e54fe5d
Marco <marcoaremail@gmail.com>
2016-06-26 02:32:25 -0300

    Added plane functionality for color and material picking

--- shape/box.cc
+++ shape/box.cc
@@ -1,9 +1,17 @@
 #include "../geometry/intersection.h"
+#include "../geometry/parametric.h"
 #include "box.h"
 
 namespace Shape {
 
-    bool Box::intersectLine (const Geometry::Line &line, Geometry::Vec<3> &normal_min, Geometry::Vec<3> &normal_max, float_max_t &t_min, float_max_t &t_max, bool fix_normals) const {
+    bool Box::intersectLine (
+        const Geometry::Line &line,
+        Geometry::Vec<3> &normal_min, Geometry::Vec<3> &normal_max,
+        Pigment::Color &color_min, Pigment::Color &color_max,
+        Light::Material &material_min, Light::Material &material_max,
+        float_max_t &t_min, float_max_t &t_max,
+        bool fix_normals
+    ) const {
 
         unsigned axis_t_min, axis_t_max;
         bool is_t_min_box_min, is_t_max_box_min;
@@ -14,21 +22,75 @@ namespace Shape {
             t_min, axis_t_min, is_t_min_box_min,
             t_max, axis_t_max, is_t_max_box_min
         )) {
+
+            const Pigment::Texture *texture = this->getTexture();
+            const Light::Surface *surface = this->getSurface();
+
+            const std::array<Geometry::Plane, 6> &planes = this->getPlanes();
+
+            const Geometry::Vec<3>
+                point_min = line.at(t_min),
+                point_max = line.at(t_max);
+
+            Geometry::Vec<2> param_min, param_max;
+
             if (axis_t_min == 0) {
-                normal_min = is_t_min_box_min ? Geometry::Vec<3>({ -1.0, 0.0, 0.0 }) : Geometry::Vec<3>({ 1.0, 0.0, 0.0 });
+                if (is_t_min_box_min) {
+                    param_min = Geometry::Parametric::Plane(planes[0], point_min);
+                    normal_min = Geometry::Vec<3>({ -1.0, 0.0, 0.0 });
+                } else {
+                    param_min = Geometry::Parametric::Plane(planes[1], point_min);
+                    normal_min = Geometry::Vec<3>({ 1.0, 0.0, 0.0 });
+                }
             } else if (axis_t_min == 1) {
-                normal_min = is_t_min_box_min ? Geometry::Vec<3>({ 0.0, -1.0, 0.0 }) : Geometry::Vec<3>({ 0.0, 1.0, 0.0 });
+                if (is_t_min_box_min) {
+                    param_min = Geometry::Parametric::Plane(planes[2], point_min);
+                    normal_min = Geometry::Vec<3>({ 0.0, -1.0, 0.0 });
+                } else {
+                    param_min = Geometry::Parametric::Plane(planes[3], point_min);
+                    normal_min = Geometry::Vec<3>({ 0.0, 1.0, 0.0 });
+                }
             } else {
-                normal_min = is_t_min_box_min ? Geometry::Vec<3>({ 0.0, 0.0, -1.0 }) : Geometry::Vec<3>({ 0.0, 0.0, 1.0 });
+                if (is_t_min_box_min) {
+                    param_min = Geometry::Parametric::Plane(planes[4], point_min);
+                    normal_min = Geometry::Vec<3>({ 0.0, 0.0, -1.0 });
+                } else {
+                    param_min = Geometry::Parametric::Plane(planes[5], point_min);
+                    normal_min = Geometry::Vec<3>({ 0.0, 0.0, 1.0 });
+                }
             }
 
             if (axis_t_max == 0) {
-                normal_max = is_t_max_box_min ? Geometry::Vec<3>({ -1.0, 0.0, 0.0 }) : Geometry::Vec<3>({ 1.0, 0.0, 0.0 });
+                if (is_t_max_box_min) {
+                    param_max = Geometry::Parametric::Plane(planes[0], point_max);
+                    normal_max = Geometry::Vec<3>({ -1.0, 0.0, 0.0 });
+                } else {
+                    param_max = Geometry::Parametric::Plane(planes[1], point_max);
+                    normal_max = Geometry::Vec<3>({ 1.0, 0.0, 0.0 });
+                }
             } else if (axis_t_max == 1) {
-                normal_max = is_t_max_box_min ? Geometry::Vec<3>({ 0.0, -1.0, 0.0 }) : Geometry::Vec<3>({ 0.0, 1.0, 0.0 });
+                if (is_t_max_box_min) {
+                    param_max = Geometry::Parametric::Plane(planes[2], point_max);
+                    normal_max = Geometry::Vec<3>({ 0.0, -1.0, 0.0 });
+                } else {
+                    param_max = Geometry::Parametric::Plane(planes[3], point_max);
+                    normal_max = Geometry::Vec<3>({ 0.0, 1.0, 0.0 });
+                }
             } else {
-                normal_max = is_t_max_box_min ? Geometry::Vec<3>({ 0.0, 0.0, -1.0 }) : Geometry::Vec<3>({ 0.0, 0.0, 1.0 });
+                if (is_t_max_box_min) {
+                    param_max = Geometry::Parametric::Plane(planes[4], point_max);
+                    normal_max = Geometry::Vec<3>({ 0.0, 0.0, -1.0 });
+                } else {
+                    param_max = Geometry::Parametric::Plane(planes[5], point_max);
+                    normal_max = Geometry::Vec<3>({ 0.0, 0.0, 1.0 });
+                }
             }
+
+            color_min = texture->at(param_min, point_min);
+            color_max = texture->at(param_max, point_max);
+
+            material_min = surface->at(param_min, point_min);
+            material_max = surface->at(param_max, point_max);
 
             if (fix_normals) {
                 normal_min.oppose(line.getDirection());
--- shape/box.h
+++ shape/box.h
@@ -1,9 +1,10 @@
-#ifndef MODULE_GRAPHICS_GEOMETRY_BOX_H_
-#define MODULE_GRAPHICS_GEOMETRY_BOX_H_
+#ifndef MODULE_GRAPHICS_SHAPE_BOX_H_
+#define MODULE_GRAPHICS_SHAPE_BOX_H_
 
 #include "../geometry/defaults.h"
 #include "../geometry/vec.h"
 #include "../geometry/line.h"
+#include "../geometry/plane.h"
 #include "shape.h"
 
 namespace Shape {
@@ -11,27 +12,49 @@ namespace Shape {
     class Box : public Shape {
 
         Geometry::Vec<3> min, max;
+        std::array<Geometry::Plane, 6> planes;
+
+        void updateMinPlanes (void) {
+            this->planes[0] = Geometry::Plane({ -1.0, 0.0, 0.0 }, this->getMin());
+            this->planes[2] = Geometry::Plane({ 0.0, -1.0, 0.0 }, this->getMin());
+            this->planes[4] = Geometry::Plane({ 0.0, 0.0, -1.0 }, this->getMin());
+        }
+
+        void updateMaxPlanes (void) {
+            this->planes[1] = Geometry::Plane({ 1.0, 0.0, 0.0 }, this->getMax());
+            this->planes[3] = Geometry::Plane({ 0.0, 1.0, 0.0 }, this->getMax());
+            this->planes[5] = Geometry::Plane({ 0.0, 0.0, 1.0 }, this->getMax());
+        }
 
         public:
 
-            Box (const Geometry::Vec<3> &_min, const Geometry::Vec<3> &_max) :
-                min(_min), max(_max) {}
+            Box (const Geometry::Vec<3> &_min, const Geometry::Vec<3> &_max, Pigment::Texture *_texture, Light::Surface *_surface) :
+                Shape(_texture, _surface), min(_min), max(_max) { this->updateMinPlanes(), this->updateMaxPlanes(); }
 
             inline const Geometry::Vec<3> &getMin (void) const { return this->min; }
             inline const Geometry::Vec<3> &getMax (void) const { return this->max; }
 
-            inline void setMin (const Geometry::Vec<3> &_min) { this->min = _min; }
-            inline void setMax (const Geometry::Vec<3> &_max) { this->max = _max; }
+            inline void setMin (const Geometry::Vec<3> &_min) { this->min = _min, this->updateMinPlanes(); }
+            inline void setMax (const Geometry::Vec<3> &_max) { this->max = _max, this->updateMaxPlanes(); }
+
+            const std::array<Geometry::Plane, 6> &getPlanes (void) const { return this->planes; }
 
             inline const Shape *boundingVolume (void) const override { return this; }
 
             const char *getType (void) const override { return "box"; }
 
-            inline Shape *clone (void) const override { return new Box(this->getMin(), this->getMax()); }
+            inline Shape *clone (void) const override { return new Box(this->getMin(), this->getMax(), this->getTexture(), this->getSurface()); }
 
             inline bool fit (void) const override { return true; }
 
-            bool intersectLine (const Geometry::Line &line, Geometry::Vec<3> &normal_min, Geometry::Vec<3> &normal_max, float_max_t &t_min, float_max_t &t_max, bool fix_normals = true) const override;
+            bool intersectLine (
+                const Geometry::Line &line,
+                Geometry::Vec<3> &normal_min, Geometry::Vec<3> &normal_max,
+                Pigment::Color &color_min, Pigment::Color &color_max,
+                Light::Material &material_min, Light::Material &material_max,
+                float_max_t &t_min, float_max_t &t_max,
+                bool fix_normals = true
+            ) const override;
 
     };
 };
